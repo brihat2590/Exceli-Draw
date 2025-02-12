c7b4f5aa160255d40b5497f4dce91191a512a6ac
brihat2590 <thapabrihatbir321@gmail.com>
2025-02-12 21:29:43 +0530

    completed http layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,22 @@
+import { URLSearchParams } from "url";
+import jwt from "jsonwebtoken"
+
 const JWT_SECRET=require("@repo/backend-common/secret")
 const{WebSockerServer}=require("ws");
 const wss=new WebSockerServer({port:8080})
 
-wss.on("connection",function connection(ws,request){
-    
+wss.on("connection",function connection(ws:any,request:any){
+    const url=request.url;
+    if(!url){
+        return;
+    }
+    const queryParams=new URLSearchParams(url.split("?")[1]);
+    const token=queryParams.get("token")||"";
+    const decoded=
 
-    })
+
+
 
 })
+
+
