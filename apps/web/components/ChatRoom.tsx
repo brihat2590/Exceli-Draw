861ceac21221affe9d47d249e1a29210e000fd1b
brihat2590 <thapabrihatbir321@gmail.com>
2025-02-21 20:58:07 +0530

    added the chatr

--- apps/web/components/ChatRoom.tsx
+++ apps/web/components/ChatRoom.tsx
@@ -1,11 +1,15 @@
-function getChat(roomId:string){
-    
+import axios from "axios"
+import { BACKEND_URL } from "../app/config"
+import { ChatRoomClient } from "./ChatRoomClient";
+
+async function getChats(roomId: string) {
+    const response = await axios.get(`${BACKEND_URL}/chats/${roomId}`);
+    return response.data.messages;
 }
 
-
-export default function ChatRoom({id}:{
-    id:string
-}){
-    
-
+export async function ChatRoom({id}: {
+    id: string
+}) {
+    const messages = await getChats(id);
+    return <ChatRoomClient id={id} messages={messages} />
 }
