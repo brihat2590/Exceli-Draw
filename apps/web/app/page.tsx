52cf9de0a804cc7ef41030da34043c2760502293
brihat2590 <thapabrihatbir321@gmail.com>
2025-02-23 22:05:36 +0530

    update 2

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,32 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const[roomId,setRoomId]=useState("")
-  const router=useRouter();
+  const [roomId, setRoomId] = useState("");
+  const router = useRouter();
+
   return (
     <div style={{
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center"
-    }} >
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw"
+    }}>
       <div>
-        <input value={roomId} type="text" placeholder="text-message" onChange={(e)=>{
-          setRoomId(e.target.value)
-        }}></input>
-        <button onClick={()=>{
-          router.push(`/room/${roomId}`)
-        }}>Join-room</button>
+        <input style={{
+          padding: 10
+        }} value={roomId} onChange={(e) => {
+          setRoomId(e.target.value);
+        }} type="text" placeholder="Room id"></input>
+
+        <button style={{padding: 10}} onClick={() => {
+          router.push(`/room/${roomId}`);
+        }}>Join room</button>
       </div>
-      
-      
     </div>
   );
 }
